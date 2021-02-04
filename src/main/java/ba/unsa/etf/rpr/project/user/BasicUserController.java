05027b05b1b03c4bf211b6999334184bfef5d86f
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-04 17:44:03 +0100

    ispravljene sve mock klase

---
+++ src/main/java/ba/unsa/etf/rpr/project/user/BasicUserController.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr.project.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/")
+public class BasicUserController {
+    private final UserService userService;
+
+    @Autowired
+    public BasicUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+}
