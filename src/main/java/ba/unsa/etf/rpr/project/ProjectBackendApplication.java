5f6a067558fc0625ecabda8cdadc1837294c05ec
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-04 14:57:25 +0100

    initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/project/ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/unsa/etf/rpr/project/ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
