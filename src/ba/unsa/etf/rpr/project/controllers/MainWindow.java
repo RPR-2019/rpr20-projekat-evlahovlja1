558b491dee4f425eea57822382082d73742a60bb
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-04 23:49:53 +0100

    malo bolje uradjena logika jsona

--- src/ba/unsa/etf/rpr/project/controllers/MainWindow.java
+++ src/ba/unsa/etf/rpr/project/controllers/MainWindow.java
@@ -1,8 +1,7 @@
 package ba.unsa.etf.rpr.project.controllers;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import ba.unsa.etf.rpr.project.dtos.User;
+import com.google.gson.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,9 +15,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class MainWindow {
-    private final String serverUrl = "http://localhost:8080";
+    private final String SERVER_URL = "http://localhost:8080";
+    private final Integer IMAGEVIEWS_SIZE = 50;
     @FXML private Button btnInitSearch;
     @FXML private HBox hboxMainContent;
 
@@ -29,16 +31,26 @@ public class MainWindow {
     private void getAllUsersFromServer() {
         runOnAnotherThread(() -> {
             try {
-                URL url = new URL(serverUrl);
+                URL url = new URL(SERVER_URL);
                 URLConnection request = url.openConnection();
                 request.connect();
 
                 // Convert to a JSON object to print data
-                JsonElement root = JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-                JsonArray rootobj = root.getAsJsonArray();
-                for (JsonElement j : rootobj) {
-                    System.out.println(j);
+                JsonElement allUsersJson = JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent()));
+                JsonArray allUsersJsonArray = allUsersJson.getAsJsonArray();
+
+                Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class,
+                        (JsonDeserializer<LocalDate>) (jsonElement, type, jsonDeserializationContext) ->
+                                LocalDate.parse(jsonElement.getAsJsonPrimitive().getAsString()))
+                        .create();
+
+                ArrayList<User> users = new ArrayList<>();
+
+                for (JsonElement j : allUsersJsonArray) {
+                    users.add(gson.fromJson(j, User.class));
                 }
+
+                System.out.println(users);
             }
             catch (Exception e) {
                 e.printStackTrace();
@@ -52,14 +64,14 @@ public class MainWindow {
         getAllUsersFromServer();
         hboxMainContent.getChildren().remove(btnInitSearch);
         ImageView x = new ImageView(new Image("/images/x.png"));
-        x.setFitHeight(50);
-        x.setFitWidth(50);
+        x.setFitHeight(IMAGEVIEWS_SIZE);
+        x.setFitWidth(IMAGEVIEWS_SIZE);
         StackPane usersDisplay = new StackPane();
         usersDisplay.getChildren().add(new Label("Elvir"));
         usersDisplay.getChildren().add(new Label("Sanelaaaaa"));
         ImageView checkmark = new ImageView(new Image("/images/checkmark.png"));
-        checkmark.setFitHeight(50);
-        checkmark.setFitWidth(50);
+        checkmark.setFitHeight(IMAGEVIEWS_SIZE);
+        checkmark.setFitWidth(IMAGEVIEWS_SIZE);
         hboxMainContent.getChildren().addAll(x, usersDisplay, checkmark);
         checkmark.setOnMouseClicked(mouseEvent -> usersDisplay.getChildren().remove(usersDisplay.getChildren().size()-1));
     }
