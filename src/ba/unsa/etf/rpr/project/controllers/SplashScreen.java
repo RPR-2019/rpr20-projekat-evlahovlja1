0cb6f14ec947508801afd14a659e6804b9de8ba1
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-06 14:21:45 +0100

    otvaranje register prozora

--- src/ba/unsa/etf/rpr/project/controllers/SplashScreen.java
+++ src/ba/unsa/etf/rpr/project/controllers/SplashScreen.java
@@ -3,6 +3,11 @@ package ba.unsa.etf.rpr.project.controllers;
 import ba.unsa.etf.rpr.project.dtos.User;
 import ba.unsa.etf.rpr.project.utilities.Json;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -27,5 +32,16 @@ public class SplashScreen {
 
         String userId = Json.sendRequest("http://localhost:8080", user.getJsonFormat());
         user.setId((long) Integer.parseInt(userId));
+    }
+
+    public void registerAction(ActionEvent actionEvent) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/registerWindow.fxml"));
+//        loader.setController(new RegisterWindow());
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Registracija");
+        stage.setScene(new Scene(root));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/ikona.jpg")));
+        stage.show();
     }
 }
