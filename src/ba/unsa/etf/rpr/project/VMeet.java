d65b7863cb06e91f405950dd564ddba369485e2b
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-08 18:52:01 +0100

    prozor za registraciju poboljsan

--- src/ba/unsa/etf/rpr/project/VMeet.java
+++ src/ba/unsa/etf/rpr/project/VMeet.java
@@ -11,7 +11,7 @@ public class VMeet extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/splashScreen.fxml"));
         primaryStage.setTitle("VMeet");
         primaryStage.setScene(new Scene(root));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/ikona.jpg")));
