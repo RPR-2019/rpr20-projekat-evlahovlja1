c1ddbd93ca673f707361901c34c3e0f3e882cb50
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-10 15:52:19 +0100

    obrisana nepotrebna testna metoda

--- src/ba/unsa/etf/rpr/project/controllers/SplashScreen.java
+++ src/ba/unsa/etf/rpr/project/controllers/SplashScreen.java
@@ -1,7 +1,5 @@
 package ba.unsa.etf.rpr.project.controllers;
 
-import ba.unsa.etf.rpr.project.dtos.User;
-import ba.unsa.etf.rpr.project.utilities.Json;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,34 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.time.LocalDate;
-import java.time.Period;
-
 public class SplashScreen {
 
 
     public void loginAction(ActionEvent actionEvent) throws Exception {
-        User user = new User(
-                null,
-                "test",
-                "testovic",
-                'F',
-                LocalDate.of(2000, 01, 01),
-                Period.between(LocalDate.of(2000, 01, 01), LocalDate.now()).getYears(),
-                "Varvare",
-                false,
-                "ETF",
-                "C#",
-                "beze nesto"
-        );
 
-        String userId = Json.sendPost("http://localhost:8080", user.getJsonFormat());
-        user.setId((long) Integer.parseInt(userId));
     }
 
     public void registerAction(ActionEvent actionEvent) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/registerWindow.fxml"));
-//        loader.setController(new RegisterWindow());
         Parent root = loader.load();
         Stage stage = new Stage();
         stage.setTitle("Registracija");
