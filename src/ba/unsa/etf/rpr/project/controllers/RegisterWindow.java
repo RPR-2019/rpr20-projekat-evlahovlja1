09190385da63ed30842c2271dc770c49e814658e
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-10 14:21:28 +0100

    male/female

--- src/ba/unsa/etf/rpr/project/controllers/RegisterWindow.java
+++ src/ba/unsa/etf/rpr/project/controllers/RegisterWindow.java
@@ -7,12 +7,17 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 public class RegisterWindow {
+    private char gender;
     @FXML private GridPane gridPaneInfo;
 
     private void addGenderRadioButtons() {
         final ToggleGroup genderRadioGroup = new ToggleGroup();
         RadioButton male = new RadioButton("M");
         male.setSelected(true);
+        male.selectedProperty().addListener((observableValue, oldVal, newVal) -> {
+            if (newVal) gender = 'M';
+            else gender = 'F';
+        });
         male.setToggleGroup(genderRadioGroup);
         RadioButton female = new RadioButton("F");
         female.setToggleGroup(genderRadioGroup);
@@ -26,5 +31,9 @@ public class RegisterWindow {
 
     @FXML public void initialize() {
         finishGui();
+    }
+
+    public void okAction() {
+
     }
 }
