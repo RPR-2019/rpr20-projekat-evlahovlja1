13105d120991bb2314f45abdeda45978cdb77989
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-15 21:52:51 +0100

    testni korisnik za potrebe testiranja napravljen u Bean-u

--- src/main/java/ba/unsa/etf/rpr/project/user/UserConfig.java
+++ src/main/java/ba/unsa/etf/rpr/project/user/UserConfig.java
@@ -1,20 +1,20 @@
-//package ba.unsa.etf.rpr.project.user;
-//
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//@Configuration
-//public class UserConfig {
-//    @Bean
-//    CommandLineRunner commandLineRunner(UserRepository userRepository) {
-//        return args -> {
-//            User a = new User("Elvir", "Vlahovljak", 'M', LocalDate.of(2000, 1, 24), "Sarajevo", true, "ETF", "C#", "nista posebno");
-//            userRepository.saveAll(List.of(a));
-//        };
-//    }
-//}
+package ba.unsa.etf.rpr.project.user;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class UserConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository) {
+        return args -> {
+            User a = new User("test", "test", 'M', LocalDate.of(2000, 1, 1), "Sarajevi", true, "test", "C#", "test");
+            userRepository.saveAll(List.of(a));
+        };
+    }
+}
