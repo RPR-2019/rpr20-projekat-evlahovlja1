717bde2f5447dedc12f55a06b89c557c59bca6e7
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-02-16 21:54:28 +0100

    napisan test za pocetno stanje nevalidnih polja

--- test/ba/unsa/etf/rpr/project/controllers/RegisterWindowTest.java
+++ test/ba/unsa/etf/rpr/project/controllers/RegisterWindowTest.java
@@ -4,25 +4,48 @@ package ba.unsa.etf.rpr.project.controllers;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(ApplicationExtension.class)
 public class RegisterWindowTest {
     private Stage stage;
+
+
     @Start
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/registerWindow.fxml"));
         Parent root = loader.load();
-        stage.setTitle("Država");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setScene(new Scene(root));
         stage.setResizable(false);
         stage.show();
         stage.toFront();
         this.stage = stage;
+    }
+
+    public static boolean hasStyle(TextField fld, String style) {
+        for (String s : fld.getStyleClass())
+            if (s.equals(style)) return true;
+        return false;
+    }
+
+    @Test
+    public void initialInvalidFieldsStyle(FxRobot fxRobot) {
+        assertAll(
+                () -> assertTrue(hasStyle(fxRobot.lookup("#txtFldUsername").queryAs(TextField.class), "invalid-field")),
+                () -> assertTrue(hasStyle(fxRobot.lookup("#pswdFldPassword").queryAs(PasswordField.class), "invalid-field")),
+                () -> assertTrue(hasStyle(fxRobot.lookup("#txtFldName").queryAs(TextField.class), "invalid-field")),
+                () -> assertTrue(hasStyle(fxRobot.lookup("#txtFldLastname").queryAs(TextField.class), "invalid-field")),
+                () -> assertTrue(hasStyle(fxRobot.lookup("#txtFldInstitution").queryAs(TextField.class), "invalid-field"))
+        );
     }
 }
