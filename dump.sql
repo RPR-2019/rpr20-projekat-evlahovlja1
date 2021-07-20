49dfa1c069ff9518ce844794fdfbf70ff66a82e3
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-07-20 09:58:06 +0200

    pocetni dump.sql gotov
    samo user tabela napravljena

---
+++ dump.sql
@@ -0,0 +1,14 @@
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    firstname TEXT,
+    lastname TEXT,
+    gender TEXT,
+    dateOfBirth TEXT,
+    city TEXT,
+    student INTEGER,
+    institution TEXT,
+    currentLanguage TEXT,
+    favoriteLanguage TEXT,
+    about TEXT
+);
